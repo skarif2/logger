9e00e2227f27f40f534100de23a837e7a73c357e
Sk Arif <skarif2@gmail.com>
2019-01-03 01:51:49 +0600

    removed chalk dependency

--- example/app.js
+++ example/app.js
@@ -7,8 +7,12 @@ const logger = require('..')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use(logger({}))
+app.use(logger())
 
 app.get('/', (req, res) => res.send('OK'))
+app.post('/', (req, res) => res.json({
+  status: 'success',
+  message: 'it was a success'
+}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- index.js
+++ index.js
@@ -10,7 +10,6 @@
  * require dependencies
  * @private
  */
-var chalk = require('chalk')
 var consola = require('consola')
 var onFinished = require('on-finished')
 
@@ -27,7 +26,13 @@ function requestLogger (options) {
     var response = res.app.response.send
     res.app.response.send = function send(body) {
       response.call(this, body)
-      res.body = body
+      if (body) {
+        try {
+          res.body = JSON.parse(body)
+        } catch (e) {
+          res.body = body
+        }
+      }
     }
     onFinished(res, function (err, res) {
       log(err, req, res)
@@ -46,17 +51,15 @@ function log(err, req, res) {
     var resTime = (diff[0] * 1e3 + diff[1] * 1e-6).toFixed(2)
 
     var reqObj = {
-      headers: req.headers,
-      params: req.params,
-      query: req.query,
-      body: req.body
+      headers: req.headers || {},
+      params: req.params || {},
+      query: req.query || {},
+      body: req.body || {}
     }
     var resObj = {
       statusCode: res.statusCode
     }
-    try {
-      resObj.body = JSON.parse(res.body)
-    } catch (e) {
+    if (res.body) {
       resObj.body = res.body
     }
 
@@ -66,69 +69,64 @@ function log(err, req, res) {
     consola.info('req:', reqObj)
     consola.info('res:', resObj)
     if (res.statusCode >= 400 && res.statusCode < 500) {
-      consola.warn(resObj.body)
+      consola.warn(resObj.body || res.statusMessage)
     } else if (res.statusCode >= 500) {
-      consola.error(resObj.body)
+      consola.error(resObj.body || res.statusMessage)
     }
-    consola.log(`${getRequestColor(res.statusCode, req.httpVersion)} ${
-      getMethodColor(req.method)} ${req.originalUrl} - ${
-      getStatusColor(res.statusCode)} - ${resTime} ms`)
+    consola.log(coloredRequest(res.statusCode, req.httpVersion)
+      + ' ' + coloredMethod(req.method) + ' ' + req.originalUrl + ' - '
+      + coloredStatus(res.statusCode) + ' - ' + resTime + ' ms')
   }
 }
 
 /**
- * get colored method name
+ * get colored method
  * @private
  */
-function getMethodColor (method) {
-  switch (method) {
-    case 'GET':
-      return chalk.green.bold('GET')
-    case 'POST':
-      return chalk.yellow.bold('POST')
-    case 'PUT':
-      return chalk.blue.bold('PUT')
-    case 'PATCH':
-      return chalk.blue.bold('PATCH')
-    case 'DELETE':
-      return chalk.red.bold('DELETE')
-    default:
-      return chalk.cyan.bold(method)
-  }
+function coloredMethod (method) {
+  return ({
+    get: '\x1b[32m',
+    head: '\x1b[32m',
+    post: '\x1b[33m',
+    put: '\x1b[34m',
+    delete: '\x1b[31m',
+    connect: '\x1b[39m',
+    options: '\x1b[36m',
+    trace: '\x1b[39m',
+    patch: '\x1b[34m'
+  }[method.toLowerCase()] || '\x1b[39m')
+  + method
+  + '\x1b[49m\x1b[39m'
 }
 
 /**
  * get http version with background color
  * @private
  */
-function getRequestColor (status, httpVersion) {
-  if (status < 300) {
-    return chalk.black.bgGreen(` HTTP/${httpVersion} `)
-  }
-  if (status >= 300 && status < 400) {
-    return chalk.black.bgBlue(` HTTP/${httpVersion} `)
-  }
-  if (status >= 400 && status < 500) {
-    return chalk.black.bgYellow(` HTTP/${httpVersion} `)
-  }
-  return chalk.black.bgRed(` HTTP/${httpVersion} `)
+function coloredRequest (status, httpVersion) {
+  return ({
+    2: '\x1b[42m',
+    3: '\x1b[44m',
+    4: '\x1b[43m',
+    5: '\x1b[41m'
+  }[status / 100 | 0] || '\x1b[49m')
+  + '\x1b[30m HTTP/' + httpVersion + ' '
+  + '\x1b[49m\x1b[39m'
 }
 
 /**
  * get colored status
  * @private
  */
-function getStatusColor (status) {
-  if (status < 300) {
-    return chalk.green(status)
-  }
-  if (status >= 300 && status < 400) {
-    return chalk.blue(status)
-  }
-  if (status >= 400 && status < 500) {
-    return chalk.yellow(status)
-  }
-  return chalk.red(status)
+function coloredStatus (status) {
+  return ({
+    2: '\x1b[32m',
+    3: '\x1b[34m',
+    4: '\x1b[33m',
+    5: '\x1b[31m'
+  }[status / 100 | 0] || '\x1b[39m')
+  + status
+  + '\x1b[39m'
 }
 
 /**
