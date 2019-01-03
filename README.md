46259a28e426da48dc15bf808975cfc0084c4abe
Sk Arif <skarif2@gmail.com>
2019-01-04 04:19:47 +0600

    added readme.md

---
+++ README.md
@@ -0,0 +1,59 @@
+# logger
+
+[![Build Status](https://travis-ci.com/skarif2/logger.svg?branch=master)](https://travis-ci.com/skarif2/logger)
+[![dependencies Status](https://david-dm.org/skarif2/logger/status.svg)](https://david-dm.org/skarif2/logger)
+[![Maintainability](https://api.codeclimate.com/v1/badges/2a0c31825e520752692b/maintainability)](https://codeclimate.com/github/skarif2/logger/maintainability)
+[![License](https://img.shields.io/badge/license-MIT-brightgreen.svg)](https://img.shields.io/badge/license-MIT-brightgreen.svg)
+
+Logger is a express middleware that can be used to log request details to the console
+
+## Usages
+
+### Enable logger for all requests
+```
+var express = require('express')
+var logger = require('@skarif2/logger)
+var app = express()
+
+app.use(express.json())
+
+app.use(logger())
+
+app.get('/', function (req, res) {
+  res.json({
+    msg: 'Beautiful log in the console!'
+  })
+})
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000')
+})
+```
+
+### Enable logger for a single route
+```
+var express = require('express')
+var logger = require('@skarif2/logger)
+var app = express()
+
+app.use(express.json())
+
+app.get('/', logger(), function (req, res) {
+  res.json({
+    msg: 'Beautiful log in the console!'
+  })
+})
+
+app.get('/no-log', function (req, res) {
+  res.json({
+    msg: 'Without any logging!'
+  })
+})
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000')
+})
+```
+
+**Note:** One must use a parser (ie: [body-parser](https://www.npmjs.com/package/body-parser)) before using logger.
+
--- index.js
+++ index.js
@@ -16,6 +16,7 @@ var onFinished = require('on-finished')
 function logger (req, res, next) {
   req.startAt = process.hrtime()
   var response = res.app.response.send
+
   res.app.response.send = function send(body) {
     response.call(this, body)
     if (body) {
@@ -26,9 +27,11 @@ function logger (req, res, next) {
       }
     }
   }
+
   onFinished(res, function (err, res) {
     log(err, req, res)
   })
+
   next()
 }
 
@@ -37,20 +40,26 @@ function log (err, req, res) {
     consola.warn(err)
     return
   }
+
   var diff = process.hrtime(req.startAt)
-  res.responseTime = (diff[0] * 1e3 + diff[1] * 1e-6).toFixed(2)
+  var time = diff[0] * 1e3 + diff[1] * 1e-6
+  res.responseTime = time.toFixed(2)
+
   var reqObj = {
     headers: req.headers || {},
     params: req.params || {},
     query: req.query || {},
     body: req.body || {}
   }
+
   var resObj = {
     statusCode: res.statusCode
   }
+
   if (res.body) {
     resObj.body = res.body
   }
+
   logObj('req', res.statusCode, reqObj)
   logObj('res', res.statusCode, resObj)
   logError(res)
@@ -62,7 +71,7 @@ function log (err, req, res) {
  * @private
  */
 function logObj (type, status, obj) {
-  consola.log(_sign(status)
+  consola.log(coloredSign(status)
     + ' '
     + type
     + ': '
@@ -78,10 +87,19 @@ function logObj (type, status, obj) {
  * @private
  */
 function logError (res) {
-  var body = res.body ? res.body : res.statusMessage
-  if ((res.statusCode / 100 | 0) === 4) {
+  var status = res.statusCode / 100 | 0
+
+  if (status < 4) {
+    return
+  }
+
+  var body = res.body 
+    ? res.body
+    : res.statusMessage
+
+  if (status === 4) {
     consola.warn(body)
-  } else if ((res.statusCode / 100 | 0) === 5) {
+  } else if (status === 5) {
     consola.error(body)
   }
 }
@@ -91,13 +109,13 @@ function logError (res) {
  * @private
  */
 function logRequest (req, res) {
-  consola.log(_request(res.statusCode, req.httpVersion)
+  consola.log(coloredRequest(res.statusCode, req.httpVersion)
     + ' '
-    + _method(req.method)
+    + coloredMethod(req.method)
     + ' '
     + req.originalUrl
     + ' - '
-    + _status(res.statusCode)
+    + coloredStatus(res.statusCode)
     + ' - '
     + res.responseTime
     + ' ms'
@@ -108,21 +126,23 @@ function logRequest (req, res) {
  * get colored sign
  * @private
  */
-function _sign (status) {
+function coloredSign (status) {
   return ({
     2: '\x1b[92m',
     3: '\x1b[94m',
     4: '\x1b[93m',
     5: '\x1b[91m'
   }[status / 100 | 0] || '\x1b[39m')
-  + '\x1b[1m✔\x1b[0m'
+  + '\x1b[1m'
+  + '✔'
+  + '\x1b[0m'
 }
 
 /**
  * get colored method
  * @private
  */
-function _method (method) {
+function coloredMethod (method) {
   return ({
     get: '\x1b[32m',
     head: '\x1b[32m',
@@ -142,14 +162,16 @@ function _method (method) {
  * get http version with background color
  * @private
  */
-function _request (status, httpVersion) {
+function coloredRequest (status, httpVersion) {
   return ({
     2: '\x1b[42m',
     3: '\x1b[44m',
     4: '\x1b[43m',
     5: '\x1b[41m'
   }[status / 100 | 0] || '\x1b[49m')
-  + '\x1b[30m HTTP/' + httpVersion + ' '
+  + '\x1b[30m HTTP/'
+  + httpVersion
+  + ' '
   + '\x1b[0m'
 }
 
@@ -157,7 +179,7 @@ function _request (status, httpVersion) {
  * get colored status
  * @private
  */
-function _status (status) {
+function coloredStatus (status) {
   return ({
     2: '\x1b[32m',
     3: '\x1b[34m',
